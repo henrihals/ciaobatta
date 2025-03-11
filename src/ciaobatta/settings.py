dd8f62615d2cd093b176efe32120a913740bae6d
henrihals <halshenri@gmail.com>
2025-03-11 15:33:10 +0100

    add static

--- src/ciaobatta/settings.py
+++ src/ciaobatta/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+APP_STATIC_DIRS = [
+    # BASE_DIR / 'homepage' / 'static',
+]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+] + APP_STATIC_DIRS
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
